99e98671c50b68432d61befac835ecc7f1f88104
Kocsis Viktor <achilleus88888@gmail.com>
2016-05-02 21:14:43 +0200

    Save button implementation, reformat phone string method

--- RegularExpressions/RegularExpressions/Form1.Designer.cs
+++ RegularExpressions/RegularExpressions/Form1.Designer.cs
@@ -28,62 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtName = new System.Windows.Forms.Label();
-            this.txtPhone = new System.Windows.Forms.Label();
-            this.txtEmail = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.buttonSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(45, 28);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(45, 68);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(38, 13);
+            this.labelPhone.TabIndex = 1;
+            this.labelPhone.Text = "Phone";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(45, 109);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(35, 13);
+            this.labelEmail.TabIndex = 2;
+            this.labelEmail.Text = "E-mail";
+            // 
             // txtName
             // 
-            this.txtName.AutoSize = true;
-            this.txtName.Location = new System.Drawing.Point(45, 28);
+            this.txtName.Location = new System.Drawing.Point(104, 28);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(35, 13);
-            this.txtName.TabIndex = 0;
-            this.txtName.Text = "Name";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 3;
             // 
             // txtPhone
             // 
-            this.txtPhone.AutoSize = true;
-            this.txtPhone.Location = new System.Drawing.Point(45, 68);
+            this.txtPhone.Location = new System.Drawing.Point(104, 68);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(38, 13);
-            this.txtPhone.TabIndex = 1;
-            this.txtPhone.Text = "Phone";
+            this.txtPhone.Size = new System.Drawing.Size(149, 20);
+            this.txtPhone.TabIndex = 4;
             // 
             // txtEmail
             // 
-            this.txtEmail.AutoSize = true;
-            this.txtEmail.Location = new System.Drawing.Point(45, 109);
+            this.txtEmail.Location = new System.Drawing.Point(104, 109);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(35, 13);
-            this.txtEmail.TabIndex = 2;
-            this.txtEmail.Text = "E-mail";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(104, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 20);
-            this.textBox1.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(104, 68);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 20);
-            this.textBox2.TabIndex = 4;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(104, 109);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(149, 20);
-            this.textBox3.TabIndex = 5;
+            this.txtEmail.Size = new System.Drawing.Size(149, 20);
+            this.txtEmail.TabIndex = 5;
             // 
             // buttonSave
             // 
@@ -93,6 +93,7 @@
             this.buttonSave.TabIndex = 6;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // Form1
             // 
@@ -100,12 +101,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.buttonSave);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.txtPhone);
             this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelName);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -115,12 +116,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label txtName;
-        private System.Windows.Forms.Label txtPhone;
-        private System.Windows.Forms.Label txtEmail;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Button buttonSave;
     }
 }
--- RegularExpressions/RegularExpressions/Form1.cs
+++ RegularExpressions/RegularExpressions/Form1.cs
@@ -17,5 +17,33 @@ namespace RegularExpressions
         {
             InitializeComponent();
         }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtName.Text, @"^([A-Za-z]*\s*)*$"))
+            {
+                MessageBox.Show("The name is invalid (only alphabetical characters are allowed)");
+            }
+            if (!Regex.IsMatch(txtPhone.Text, @"^((\(\d{3}\)?)|(\d{3}-))?\d{3}-\d{4}$"))
+            {
+                MessageBox.Show("The phone number is not a valid US phone number");
+            }
+            if (!Regex.IsMatch(txtEmail.Text, @"^([a-zA-Z0-9_\-" +
+                                              @"\.]+)@((\[[0-9]{1,3}" +
+                                              @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                                              @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"))
+            {
+                MessageBox.Show("The e-mail address is not valid.");
+            }
+            txtPhone.Text = ReformatPhone(txtPhone.Text);
+        }
+        static string ReformatPhone(string phonenumber)
+        {
+            Match m = Regex.Match(phonenumber, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
+            return String.Format("({0}) {1}-{2}",
+                                m.Groups[1],
+                                m.Groups[2],
+                                m.Groups[3]);
+        }
     }
 }
