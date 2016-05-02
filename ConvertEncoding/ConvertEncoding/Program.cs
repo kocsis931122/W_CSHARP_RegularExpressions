98d975d3ed48c1b7e7cb4ed6f5a096e2e0947757
Kocsis Viktor <achilleus88888@gmail.com>
2016-05-02 21:33:16 +0200

    Convert encoding example

---
+++ ConvertEncoding/ConvertEncoding/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertEncoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\dev\ass22-utf8.txt");
+            StreamWriter sw = new StreamWriter("ass22-utf7.txt", false, Encoding.UTF7);
+
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
