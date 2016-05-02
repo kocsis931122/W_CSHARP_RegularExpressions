af80e1947504d920dbced1b1a68b7856f742b941
Kocsis Viktor <achilleus88888@gmail.com>
2016-05-02 22:44:57 +0200

    complete simpleThreadingDemo project

--- SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -11,10 +11,25 @@ namespace SimpleThreadingDemo
     {
         static void Main(string[] args)
         {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+
+            first.Start();
+            second.Start();
+
+            first.Join();
+            second.Join();
+
+            Console.Read();
         }
         static void Counting()
         {
-
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread: {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
         }
     }
 }
